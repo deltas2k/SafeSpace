606bda45426f093f5d4e27885623ef3176331350
Matthew O <deltas2k@gmail.com>
2019-11-16 10:21:29 -0700

    graph created

---
+++ SafeSpace/SafeSpace/GraphViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GraphViewController.swift
+//  SafeSpace
+//
+//  Created by Matthew O'Connor on 11/15/19.
+//  Copyright © 2019 Matthew O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
